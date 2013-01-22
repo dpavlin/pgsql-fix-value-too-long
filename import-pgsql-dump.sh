73559514f564de7166dbd2b45e350e04731d96dd
Dobrica Pavlinusic <dpavlin@rot13.org>
2013-01-22 20:02:34 +0100

    restart import if last run produced errors

--- import-pgsql-dump.sh
+++ import-pgsql-dump.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -xe
 
+echo started on `date`
 touch errors
 (
 psql -l | grep ibatisti | awk '{ print $1 }' | xargs -i dropdb {}
 ./pgsql-fix-value-too-long.pl errors $1 | \
 ( psql --echo-queries template1 2>&1 )
 2>&1 ) | tee log
-grep -A 1 'value too long' log | tee -a errors
+grep -A 1 'value too long' log | tee -a errors 
+mv log log.last
+grep 'value too long' log.last && exec $0 $*
